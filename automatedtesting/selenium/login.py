92af71d79868fa0fc48d530a79ba42cc3b6cb2f7
Okay, Enis SITI-PTIV/IB/1 <Enis.Okay@shell.com>
2025-01-02 23:11:05 +0100

    changed yaml

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,14 +7,14 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login(user, password):
     print("Starting the browser...")
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
     print("Browser started successfully. Navigating to the demo page to login.")
     driver.get("https://www.saucedemo.com/")
 
-    # Login işlemi
+
     driver.find_element("id", "user-name").send_keys(user)
     driver.find_element("id", "password").send_keys(password)
     driver.find_element("id", "login-button").click()
