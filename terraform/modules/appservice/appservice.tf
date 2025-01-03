cba9bce6023f41cc93c23f38c39b645d324da522
Okay, Enis SITI-PTIV/IB/1 <Enis.Okay@shell.com>
2025-01-03 13:48:30 +0100

    changed yaml

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   os_type             = "Linux"
-  sku_name            = "P1V2"
+  sku_name            = "P1v2"
 }
 
 resource "azurerm_linux_web_app" "test" {
